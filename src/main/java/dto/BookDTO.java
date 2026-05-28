d3b4046d336f2f94e122354170791777d28f1e47
CHOI SUMIN <bugang040309@gmail.com>
2026-05-28 15:31:48 +0900

    Create BookDTO.java

---
+++ src/main/java/dto/BookDTO.java
@@ -0,0 +1,14 @@
+package dto;
+
+public class BookDTO {
+
+    private int bookId;
+    private String title;
+    private String author;
+    private String publisher;
+    private String isbn;
+    private String category;
+    private int totalCount;
+    private int availableCount;
+
+}
