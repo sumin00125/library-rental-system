ba19422af40a6737bbf8fed621bd4373f751aa05
CHOI SUMIN <bugang040309@gmail.com>
2026-05-29 10:00:50 +0900

    Create BookController.java

---
+++ src/main/java/controller/BookController.java
@@ -0,0 +1,47 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/book")
+
+public class BookController extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    public BookController() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String action = request.getParameter("action");
+
+        if(action == null) {
+
+            response.sendRedirect("index.jsp");
+
+        } else if(action.equals("list")) {
+
+            response.sendRedirect("bookList.jsp");
+
+        } else if(action.equals("form")) {
+
+            response.sendRedirect("bookForm.jsp");
+        }
+    }
+
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        doGet(request, response);
+    }
+}
